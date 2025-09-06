77ba182d48e3de5a3bd52944f3e9ec751d1c2f40
Berisha-7k <gokcan.srma@proton.me>
2025-09-06 14:28:51 +0300

    Testt

--- RubberDuck/sway/scripts/sway-bar-update.sh
+++ RubberDuck/sway/scripts/sway-bar-update.sh
@@ -13,33 +13,18 @@ player_status=$(playerctl --player=spotify status)
 updates=$(checkupdates 2>/dev/null | wc -l)
 
 ###Brightness
-brightness=$(/home/x3ck/Documents/scripts/brightness.sh)
+brightness=$(/etc/sway/scripts/brightness.sh)
 
 #Volume
-volume=$(/home/x3ck/Documents/scripts/volume.sh)
+volume=$(/etc/sway/scripts/volume.sh)
 
 ### Greetings
-greeting=$(hour=`date +%H`
-if [ $hour -lt 12 ] # if hour is less than 12
-then
-echo "GOOD MORNING $USER"
-elif [ $hour -le 16 ] # if hour is less than equal to 16
-then
-echo "GOOD AFTERNOON $USER"
-elif [ $hour -le 20 ] # if hour is less than equal to 20
-then
-echo "GOOD EVENING $USER"
-else
-echo "GOOD NIGHT $USER"
-fi
-)
-
-
+greeting=$(/etc/sway/scripts/greetings.sh)
 ### Weather
 weather=$(cat ~/.cache/weather.json | jq '.main.temp' | awk '{print int($1+0.5)}')
 ##
 weather_loc=$(cat ~/.cache/weather.json | jq -r '.name')
 ###
-w_icon=$(/home/x3ck/Documents/scripts/Capella/scripts/weather-text-icon)
+w_icon=$(/etc/sway/scripts/weather-text-icon)
 
 echo " $greeting |   $song_status $media_artist - $media_song |                                           | $w_icon $weather °C $weather_loc | $volume | $brightness |   $updates |  $date  $current_time "
